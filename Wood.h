39a716fb2ed1b920bbb4ed8e72385edf48230da5
Tdhuyvk <trandinhhuyvk@gmail.com>
2025-06-06 21:53:03 +0700

    Fix bug in Wood

--- Wood.h
+++ Wood.h
@@ -16,4 +16,7 @@ public:
 	void Update(DWORD dt) {}
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	void RenderBoundingBox();
+
+	virtual int IsCollidable() override { return 1; }
+	virtual int IsBlocking() override { return 1; }
 };
